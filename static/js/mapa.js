680ab011946bc2c39f4977d990746636ad390c3a
Sumaia Villela <dadosparasumaia@gmail.com>
2024-12-16 01:41:13 -0300

    correção

--- static/js/mapa.js
+++ static/js/mapa.js
@@ -394,7 +394,7 @@ function getRandomUF() {
 // ========= INICIALIZAÇÃO DO MAPA =========
 
 // Token de acesso do Mapbox
-mapboxgl.accessToken = "pk.eyJ1Ijoic3VtYWlhdmlsbGVsYSIsImEiOiJjbTBlaTRmamwwcDZ6MmpvOTNvcTAydjN5In0.h_MFiNdtYcgHN5ZforR4mw";
+mapboxgl.accessToken = "sk.eyJ1Ijoic3VtYWlhdmlsbGVsYSIsImEiOiJjbTRxanByeXkxNHpjMmlvcWlqMzU2ZXhiIn0.jf2TXVDAPbGmY9ko7nxrGw";
 
 // Cria mapa
 const map = new mapboxgl.Map({
